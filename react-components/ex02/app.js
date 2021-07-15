725f57a11d7fc5e442b3599c3064729cf2020f1a
Dragos Iordache <dragosh.iordache@yahoo.com>
2021-07-15 14:17:36 +0300

    added execrises for map/reduce

---
+++ react-components/ex02/app.js
@@ -0,0 +1,33 @@
+const person = {
+  name: 'Dragos',
+  surname: 'Iordache',
+  age: 32,
+  petOwner: false,
+  skills: [
+    'html',
+    'javascript',
+    'css',
+    'java',
+    'c++',
+    'node',
+    'jquery',
+    'node.js',
+  ],
+  friends: [
+    {
+      name: 'Larry',
+      surname: 'Larryson',
+      age: 30,
+    },
+    {
+      name: 'Steven',
+      surname: 'Stevenson',
+      age: 31,
+    },
+    {
+      name: 'Carol',
+      surname: 'Carolson',
+      age: 29,
+    },
+  ],
+};
---
+++ react-components/ex04/app.js
@@ -0,0 +1,31 @@
+const person = {
+  name: 'Dragos',
+  surname: 'Iordache',
+  age: 32,
+  petOwner: true,
+  skills: {
+    html: true,
+    css: true,
+    javaScript: true,
+  },
+  friends: {
+    larry: {
+      name: 'Larry',
+      surname: 'Larryson',
+      age: 30,
+    },
+    steven: {
+      name: 'Steven',
+      surname: 'Stevenson',
+      age: 31,
+    },
+    carol: {
+      name: 'Carol',
+      age: 29,
+      surname: 'Carolson',
+    },
+  },
+};
+
+
+
