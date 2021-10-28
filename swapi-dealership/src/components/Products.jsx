54f97ad5cb7ecdfb36e12ed384e687d9a763b4d0
Valentin Tureac <valentin.tureac@gmail.com>
2021-10-28 21:20:05 +0300

    Add infinity scrolling

--- swapi-dealership/src/components/Products.jsx
+++ swapi-dealership/src/components/Products.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import ProductTile from './ProductTile';
 
 const baseUrl = 'https://swapi.dev/api/vehicles';
@@ -7,6 +7,8 @@ export const Products = () => {
   const [products, setProducts] = useState([]);
   const [busy, setBusy] = useState(true);
   const [urlToFetch, setUrlToFetch] = useState(baseUrl);
+  const nextUrl = useRef(baseUrl);
+  const loadMoreRef = useRef(null);
 
   // recipe
   const fetchProducts = useCallback(() => {
@@ -17,9 +19,16 @@ export const Products = () => {
         return response.json();
       })
       .then((data) => {
-        const products = data.results;
+        const newProducts = data.results;
 
-        setProducts(products);
+        // nextUrl.current = data.next !== null ? data.next : '';
+        // nullish coalescing operator:
+        nextUrl.current = data?.next ?? '';
+
+        // closure functions
+        setProducts((products) => {
+          return [...products, ...newProducts];
+        });
         setBusy(false);
       });
   }, [urlToFetch]);
@@ -28,6 +37,29 @@ export const Products = () => {
     fetchProducts();
   }, [fetchProducts]);
   // end recipe
+
+  useEffect(() => {
+    // defining options
+    const options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 1.0,
+    };
+    // instantiating the observer
+    const observer = new IntersectionObserver((entries) => {
+      const intersectionObserverEntry = entries[0];
+
+      if (
+        intersectionObserverEntry.isIntersecting &&
+        nextUrl.current.length > 0
+      ) {
+        setUrlToFetch(nextUrl.current);
+      }
+    }, options);
+    // listening to the intersection events (observing)
+    observer.observe(loadMoreRef.current);
+    // provide cleanup function
+  }, []);
 
   return (
     <section className="row">
@@ -41,7 +73,23 @@ export const Products = () => {
         return <ProductTile product={product} key={name}></ProductTile>;
       })}
 
-      {busy ? '...loading' : <></>}
+      <div className="col-12 text-center" ref={loadMoreRef}>
+        {nextUrl.current.length > 0 ? (
+          <button
+            className="btn btn-xl btn-warning"
+            title="Load more products"
+            type="button"
+            disabled={busy}
+            onClick={() => {
+              setUrlToFetch(nextUrl.current);
+            }}
+          >
+            {busy ? '...loading' : 'Load More'}
+          </button>
+        ) : (
+          <></>
+        )}
+      </div>
     </section>
   );
 };
